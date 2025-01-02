7c33c0cfbb712d7efb955c7a85faaedee4a371f4
近藤　望 <li2xue3mei2@gmail.com>
2025-01-02 14:17:15 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById('btn');
-console.log(button);
-
-const Label = document.getElementById('text');
-console.log(Label);
-
-button.addEventListener('click',() =>{
-  Label.textContent = 'ボタンをクリックしました。';
-});
